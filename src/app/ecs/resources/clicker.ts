e3958a0db75148f8d93b038b439c1d2cd6b34ba2
Curtis Chong <curtischong5@gmail.com>
2023-11-25 02:25:52 -0500

    added prettier

--- src/app/ecs/resources/clicker.ts
+++ src/app/ecs/resources/clicker.ts
@@ -2,5 +2,4 @@ import { ClientResourcesBuilder } from "../ecs_types";
 
 export async function addClickerResources(builder: ClientResourcesBuilder) {
     builder.addGlobal("/clicker/num_pressed", { value: 5 });
-  }
-  
+}
